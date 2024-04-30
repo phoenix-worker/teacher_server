0fa92022a4064cac97b6d876adfca4ba5979ac52
blogger <phoenix.worker@gmail.com>
2024-04-30 10:29:14 +0500

    Начальный коммит

---
+++ src/main/kotlin/com/example/plugins/Serialization.kt
@@ -0,0 +1,34 @@
+package com.example.plugins
+
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.application.install
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.response.respond
+import io.ktor.server.routing.post
+import io.ktor.server.routing.routing
+import java.io.File
+import java.io.FileWriter
+import java.text.SimpleDateFormat
+import java.util.Date
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
+    }
+    routing {
+        post("/notifyTodayTaskFinished") {
+            val dir = File("/home/blogger")
+            if (!dir.exists()) dir.mkdirs()
+            val file = File(dir, "teacherLog")
+            if (!file.exists()) file.createNewFile()
+            val fileWriter = FileWriter(file, true)
+            val simpleDateFormat = SimpleDateFormat("dd-MM-yyyy")
+            val dateString = simpleDateFormat.format(Date())
+            fileWriter.append("$dateString\n")
+            fileWriter.close()
+            call.respond(mapOf("result" to "success"))
+        }
+    }
+}
